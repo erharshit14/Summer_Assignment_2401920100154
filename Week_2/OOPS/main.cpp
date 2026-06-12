bf9c1609b2f4f180234d560a7abfeb581c68b7d5
Harshit Gupta <erharshit2025@gmail.com>
2026-06-13 00:26:08 +0530

    Week 2 Solutions

---
+++ Week_2/OOPS/main.cpp
@@ -0,0 +1,24 @@
+#include "Veena.h"
+#include "Saxophone.h"
+
+int main() {
+
+   
+    Veena veena;
+    veena.play();
+
+    
+    Saxophone saxophone;
+    saxophone.play();
+
+    
+    Playable* p;
+
+    p = &veena;
+    p->play();
+
+    p = &saxophone;
+    p->play();
+
+    return 0;
+}
